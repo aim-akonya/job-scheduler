72bdcae70eef9780778c62690f1fc71c81ba6d85
aim-akonya <akonya@adtel.co.ke>
2021-05-05 17:43:52 +0300

    feat: status

--- src/main/java/com/aim/app/Application.java
+++ src/main/java/com/aim/app/Application.java
@@ -8,13 +8,10 @@ import org.slf4j.LoggerFactory;
 
 import static org.quartz.JobBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
-import static org.quartz.CronScheduleBuilder.*;
-import static org.quartz.CalendarIntervalScheduleBuilder.*;
 import static org.quartz.TriggerBuilder.*;
+import static org.quartz.CronScheduleBuilder.*;
 
 import org.quartz.JobDetail;
-
-import static org.quartz.DateBuilder.*;
 
 public class Application {
 
@@ -30,15 +27,28 @@ public class Application {
 			LOGGER.info("scheduler instance started");
 
 			// TODO
-			JobDetail job = newJob(HelloJob.class).withIdentity("myJob", "group1").build();
+			JobDetail job = newJob(HelloJob.class).withIdentity("myJob", "group1")
+					.usingJobData("jobType", "password_expiry_notification")
+					.build();
 
-			// trigger
+			// simple trigger
 			Trigger trigger = newTrigger().withIdentity("myTrigger", "group1").startNow()
-					.withSchedule(simpleSchedule().withIntervalInSeconds(40).repeatForever()).build();
+					.withSchedule(simpleSchedule().withIntervalInSeconds(1).repeatForever()).build();
+			
+			
+			//cron trigger
+			Trigger trigger2 = newTrigger()
+				    .withIdentity("myTrigger2", "group1")
+				    .withSchedule(cronSchedule("0 0/2 8-17 * * ?"))
+				    .forJob("myJob", "group1")
+				    .build();
+			
+			
 
 			scheduler.scheduleJob(job, trigger);
+			scheduler.scheduleJob(job, trigger2);
 
-			Thread.sleep(6000);
+			Thread.sleep(60000);
 
 			scheduler.shutdown();
 			LOGGER.info("scheduler instance stopped");
--- src/main/java/com/aim/app/HelloJob.java
+++ src/main/java/com/aim/app/HelloJob.java
@@ -3,23 +3,35 @@
  */
 package com.aim.app;
 
-import java.util.Date;
 
 import org.quartz.Job;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.JobKey;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author aim
  *
  */
 public class HelloJob implements Job{
+	
+	private static Logger LOGGER = LoggerFactory.getLogger(HelloJob.class);
 
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
 		
-		System.out.print(new Date().getTime());
+		JobKey key = context.getJobDetail().getKey();
+		JobDataMap dataMap =  context.getJobDetail().getJobDataMap();
+		
+		String jobType = dataMap.getString("jobType");
+		
+		LOGGER.info("Job Instance "+key +". Job Type "+jobType);
 		
 	}
+	
+	
 
 }
