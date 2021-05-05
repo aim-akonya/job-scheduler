dcda1c6039df743dc6f26175c50001dcd553a2c2
aim-akonya <akonya@adtel.co.ke>
2021-05-05 13:14:13 +0300

    feat: start job instance

--- src/main/java/com/aim/app/Application.java
+++ src/main/java/com/aim/app/Application.java
@@ -1,8 +1,28 @@
 package com.aim.app;
 
+import org.quartz.Scheduler;
+import org.quartz.impl.StdSchedulerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Application {
+	
+	private static Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
-		System.out.println("Application Startup Reached");
+		
+		//grab the Schedular instance from the factory
+		try {
+			Scheduler scheduler =  StdSchedulerFactory.getDefaultScheduler();
+			
+			scheduler.start();
+			LOGGER.info("scheduler instance started");
+			
+			scheduler.shutdown();
+			LOGGER.info("scheduler instance stopped");
+		}catch(Exception e) {
+			LOGGER.error("error stopping schedular instance");
+			
+		}
 	}
 }
